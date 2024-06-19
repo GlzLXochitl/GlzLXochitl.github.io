4ebdeea323152e09e7102c2ce5c171f677ecc781
glzlxochitl <xochitlgl4903@gmail.com>
2024-06-19 02:25:11 -0600

    feat: intro-return

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* ANIMACION DE INTRO */
 setTimeout(function() {
     var intro = document.querySelector('#intro');
     intro.classList.add('hide');
@@ -6,3 +7,20 @@ setTimeout(function() {
         intro.style.display = 'none';
     }, 2000); // Este tiempo debe ser igual a la duración de la transición en CSS
 }, 1000);
+
+/* ANIMACION DE NAV */
+window.addEventListener("scroll", function() {
+    var nav = document.querySelector("nav");
+    nav.classList.toggle("scrolled", window.scrollY > 100);
+});
+
+/* ANIMACION DE LAS REDES DEL NAV */
+window.addEventListener('scroll', function() {
+    var socialMedia = document.querySelector('.social-media-disposition');
+    if (window.pageYOffset > 100) { // puedes ajustar este valor
+        socialMedia.classList.add('hide');
+    } else {
+        socialMedia.classList.remove('hide');
+    }
+});
+
